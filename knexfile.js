1c1f900418d879297fa20870200062c08e3b5179
dany94xd <danimaxi60@hotmail.com>
2019-01-27 21:55:47 -0500

    new

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,7 @@
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: 'postgres://wrxxebozkbzjie:3f552889987af27301244414e54f8e36ebafbe220e975c269edd9bf816ae2aa6@ec2-23-21-244-254.compute-1.amazonaws.com:5432/d5av1dursm76cb
-'
+    connection: 'postgres://wrxxebozkbzjie:3f552889987af27301244414e54f8e36ebafbe220e975c269edd9bf816ae2aa6@ec2-23-21-244-254.compute-1.amazonaws.com:5432/d5av1dursm76cb'
    //connection: 'postgres://postgres:postgres@localhost/postgres'
   },
   production: {
